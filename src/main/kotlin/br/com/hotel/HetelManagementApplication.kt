b7b11724fea47b354758d404036829209c1f70dd
Gilson Silva <gilsonsilvati@gmail.com>
2024-09-04 11:31:39 -0300

    build: setup project

---
+++ src/main/kotlin/br/com/hotel/HetelManagementApplication.kt
@@ -0,0 +1,11 @@
+package br.com.hotel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HetelManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<HetelManagementApplication>(*args)
+}
